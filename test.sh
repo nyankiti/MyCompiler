a39a7d78bc72166330360596ffa2d949c8920d86
Nyankiti <nyankiti20000824@gmail.com>
2021-05-29 17:58:58 +0900

    整数一つをコンパイルする言語の作成。テストファイルの作成。Makefileの作成

---
+++ test.sh
@@ -0,0 +1,28 @@
+# .sh はシェルスクリプトを示す
+
+
+
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+
+# 第一引数が期待される値、第二引数が入力する値として、assert関数内で指定した実行ファイルを実行してくれる（テスト）
+assert 0 0
+assert 42 42
+
+echo OK
